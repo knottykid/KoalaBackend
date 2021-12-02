a10a833fbed5b03693ef75636eefa1942ab751ac
Meng Li <meng.li@signavio.com>
2021-12-02 11:37:55 +0100

    initial commit

---
+++ src/test/java/com/koala/signavlings_BE/SignavlingsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koala.signavlings_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignavlingsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
