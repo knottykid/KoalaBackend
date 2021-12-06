a4e5843e2816a016a15c7ad9bff19c12e8943903
Meng Li <meng.li@signavio.com>
2021-12-06 11:08:20 +0100

    Modify the file to set up AWS deployment, add crossOrigin feature to controller profile

--- src/main/java/com/koala/signavlings_BE/profileController/ProfileController.java
+++ src/main/java/com/koala/signavlings_BE/profileController/ProfileController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
+@CrossOrigin
 @RequestMapping("api/profile")
 public class ProfileController {
 
