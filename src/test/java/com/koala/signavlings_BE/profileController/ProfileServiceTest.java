7609beb6c8a802e68ba38de8bd78e310ba0eb6e7
Thorat <preeti.shivaji.thorat@sap.com>
2021-12-08 14:22:14 +0100

    Added new tests

--- src/test/java/com/koala/signavlings_BE/profileController/ProfileServiceTest.java
+++ src/test/java/com/koala/signavlings_BE/profileController/ProfileServiceTest.java
@@ -14,6 +14,13 @@ class ProfileServiceTest {
         JSONAssert.assertEquals("{id:1L}", actual, false);
         JSONAssert.assertEquals("{name:\"Max Zimmermann\"}", actual, false);
         JSONAssert.assertEquals("{joiningMonth:\"December\", bio:\"I am here for testing\", role:\"Archangle\"}", actual, false);
+
+        //The following tests should fail
+        /*
+        JSONAssert.assertEquals("{photo:\"https://ca.slack-edge.com/T07DBAMS9-U02K8S7V950-5325a191b353-512\"}", actual, false);// Test with wrong URL
+        JSONAssert.assertEquals("{name:mkyong}", actual, false);//This test should fail as the name is wrong
+        JSONAssert.assertEquals("{name:\"Max Zimmermann\"}", actual, true);//This test will fail as the strict checking is true
+        */
     }
 
 }
