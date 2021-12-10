299086ac7f12d53d6395e1a95e4563b47788eb73
Luise Loparev <luise.loparev@signavio.com>
2021-12-10 09:29:52 +0100

    check code comments

--- src/main/java/com/koala/signavlings_BE/profileController/ProfileRepository.java
+++ src/main/java/com/koala/signavlings_BE/profileController/ProfileRepository.java
@@ -3,7 +3,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
-
 @Repository
 public interface ProfileRepository extends CrudRepository<Profile, Long>{
 
